d756e7eed36a523664c6c8099a8d24e609985928
JustWaveThings <justwavethings@proton.me>
2023-07-26 17:57:00 -0400

    require html files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const about = require('./about');
+const contact = require('./contact-me');
+const notFound = require('./not-found');
+const home = require('./index');
